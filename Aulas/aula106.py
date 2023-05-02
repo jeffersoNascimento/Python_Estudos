7aec9cc77aa55cae56fa5be208d8fcb77ca5511c
Jefferson Nascimento <jeffersonnn561@gmail.com>
2023-05-02 18:17:14 -0300

    salvando estudos

---
+++ Aulas/aula106.py
@@ -0,0 +1,22 @@
+# Ordem dos decoradores
+def parametros_decorador(nome):
+    def decorador(func):
+        print('Decorador:', nome)
+
+        def sua_nova_funcao(*args, **kwargs):
+            res = func(*args, **kwargs)
+            final = f'{res} {nome}'
+            return final
+        return sua_nova_funcao
+    return decorador
+
+
+@parametros_decorador(nome='terceiro')
+@parametros_decorador(nome='segundo') # Executado de baixo pra cima
+@parametros_decorador(nome='primeiro')
+def soma(x, y):
+    return x + y
+
+
+dez_mais_cinco = soma(10, 5)
+print(dez_mais_cinco)
